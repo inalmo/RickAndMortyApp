ddb996344f266a0bbcd55793cdc1a6d1071c5995
Iñigo Alonso <iamolledo@gmail.com>
2024-02-16 19:04:42 +0100

    Initial Commit

---
+++ RickMortyApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RickMortyApp
+//
+//  Created by Iñigo Alonso Molledo on 16/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RickMortyApp/RickMortyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickMortyAppApp.swift
+//  RickMortyApp
+//
+//  Created by Iñigo Alonso Molledo on 16/2/24.
+//
+
+import SwiftUI
+
+@main
+struct RickMortyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
